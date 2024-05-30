2977e27159f9f9b786cd9fdc799dd1a4b13327ab
Noah Esteves <noahesteves@gmail.com>
2024-05-30 19:15:39 +0100

    oda

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,6 @@
+import ConsultDay from "@/components/consultday";
+import Tabela from "@/components/tabela";
+
 export default function Services(){
     return(
         <div className="w-full h-fit flex flex-col items-center mt-20">
@@ -5,11 +8,28 @@ export default function Services(){
                 <span className="text-[#ea8f10] text-4xl mb-4 max-sm:text-2xl">Services</span>
                 <h1 className="font-serif font-bold text-9xl max-sm:text-5xl text-white">High-Impact Services</h1>
             </div>
-            <div className="bg-white w-full h-fit flex flex-col items-center max-sm:py-12 py-20 gap-20">
-                <div className="">
-
+            <div className="bg-white w-full h-fit flex flex-col items-center gap-10 py-44 max-sm:gap-6 max-sm:py-24">
+                <div className="w-full h-fit flex max-sm:flex-col max-sm:gap-10 px-[5%]">
+                    <div className="w-[50%] max-sm:w-full h-fit max-sm:text-center">
+                        <h1 className="max-sm:text-4xl text-8xl font-bold font-serif text-[#181818]">High-impact <br /> services to help <br /> your business</h1>
+                    </div>
+                    <div className="w-[50%] max-sm:w-full gap-4 h-fit flex flex-col">
+                        <span className="text-4xl mb-4 max-sm:text-xl text-[#181818]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, quos! Quo vitae commodi suscipit corrupti tenetur ipsa libero voluptatibus architecto sapiente debitis! Nemo quisquam laboriosam libero maxime quis fugit repellat? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum possimus debitis officiis quasi impedit veniam molestias odit consectetur. Impedit rerum, voluptates odio accusantium reprehenderit pariatur officia vel mollitia saepe totam?</span>
+                    </div>
+                </div>
+                <div className="w-full h-fit px-[5%] grid grid-cols-3 max-sm:grid-cols-1 gap-12">
+                    <Tabela/>
+                    <Tabela/>
+                    <Tabela/>
+                    <Tabela/>
+                    <Tabela/>
+                    <Tabela/>
+                    <Tabela/>
+                    <Tabela/>
+                    <Tabela/>
                 </div>
             </div>
+            <ConsultDay/>
         </div>
     )
 }
